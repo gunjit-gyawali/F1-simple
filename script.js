069b7b19a333516cc9f92873bdfc4303db714395
Gunjit Gyawali <gunjitgyawali1@gmail.com>
2026-04-08 19:58:37 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function teams(){
+    let team = document.getElementById("team").value;
+    if ( team == 11){
+        alert("Yay its correct! There are 11 teams.");
+    } else {
+        alert("Nope its wrong! There are 11 teams.");
+    }
+}
+
+function drivers(){
+    let driver = document.getElementById("driver").value;
+    if ( driver == 22){
+        alert("Yay its correct! There are 22 drivers.");
+    } else {
+        alert("Nope its wrong! There are 22 drivers.");
+    }
+}
+ 
+function max(){
+    let champion = document.getElementById("max").value;
+    if ( champion == 4){
+        alert("Yay its correct! He won from 2021 to 2024.");
+    } else {
+        alert("Nope its wrong! He won from 2021 to 2024. Which means 4 times champion.");
+    }
+}
+
+
+function lewis(){
+    let champion = document.getElementById("lewis").value;
+    if ( champion == 7){
+        alert("Yay its correct! He is 7 times world champion.");
+    } else {
+        alert("Nope its wrong! He is 7 times world champion.");
+    }
+}
